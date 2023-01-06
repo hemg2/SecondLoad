8fba8e1c34bdfc50af3ddfd29dd83bd3c2e297af
hemg2 <hemg2@naver.com>
2023-01-06 15:45:08 +0900

    UIAlertController

--- SecondUpLoad/MainView/MainViewController.swift
+++ SecondUpLoad/MainView/MainViewController.swift
@@ -8,11 +8,61 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import SnapKit
 
 class MainViewController: UIViewController {
-
+    
+    let disposeBag = DisposeBag()
+    
+    let tableView = UITableView()
+    let submitButton = UIBarButtonItem()
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil  )
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    func bind(_ viewModel: MainViewModel) {
+        
+    }
+    
+    private func attribute() {
+        title = "거래 글쓰기"
+        view.backgroundColor = .white
+        
+        submitButton.title = "제출"
+        submitButton.style = .done
+        
+        navigationItem.setRightBarButton(submitButton, animated: true)
+        
+        tableView.backgroundColor = .white
+        tableView.separatorStyle = .singleLine
+        tableView.tableFooterView = UIView()
+    }
+    
+    private func layout() {
+        view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
 
+typealias Alert = (title: String, message: String?)
+extension Reactive where Base: MainViewController {
+    var setAlert: Binder<Alert> {
+        return Binder(base) { base, data in
+            let alertController = UIAlertController(title: data.title, message: data.message, preferredStyle: .alert)
+            let action = UIAlertAction(title: "확인", style: .cancel)
+            alertController.addAction(action)
+            base.present(alertController, animated: true)
+        }
     }
 }
