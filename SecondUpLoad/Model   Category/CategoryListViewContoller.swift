eb0477e0a8eda637ff3ff7ebd722cee6e37e90c3
hemg2 <hemg2@naver.com>
2023-01-06 16:24:38 +0900

    CategoryCell

--- SecondUpLoad/Model   Category/CategoryListViewContoller.swift
+++ SecondUpLoad/Model   Category/CategoryListViewContoller.swift
@@ -18,7 +18,8 @@ class CategoryListViewContoller: UIViewController {
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         
-        
+        attribute()
+        layout()
     }
     
     required init?(coder: NSCoder) {
@@ -26,14 +27,36 @@ class CategoryListViewContoller: UIViewController {
     }
     
     func bind(_ ViewModel: CategoryViewModel) {
+        ViewModel.cellData.drive(tableView.rx.items) { tv, row, data in
+            let cell = tv.dequeueReusableCell(withIdentifier:"CategoryListCell", for: IndexPath(row: row, section: 0))
+            cell.textLabel?.text = data.name
+            return cell
+        }
+        .disposed(by: disposeBag)
         
+        ViewModel.pop
+            .emit(onNext: { [weak self] _ in
+                self?.navigationController?.popViewController(animated: true)
+            })
+            .disposed(by: disposeBag)
+        
+        tableView.rx.itemSelected.map { $0.row } .bind(to: ViewModel.itemSelected) .disposed(by: disposeBag)
     }
     
     private func attribute() {
+        view.backgroundColor = .systemBackground
         
+        tableView.backgroundColor = .white
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "CategoryListCell")
+        tableView.separatorStyle = .singleLine
+        tableView.tableFooterView = UIView()
     }
     
     private func layout() {
+        view.addSubview(tableView)
         
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
 }
