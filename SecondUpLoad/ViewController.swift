0edbcb6de5a891a6278a21ea61e0f79b40611776
hemg2 <hemg2@naver.com>
2023-01-06 14:53:15 +0900

    Initial Commit

---
+++ SecondUpLoad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondUpLoad
+//
+//  Created by 1 on 2023/01/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
