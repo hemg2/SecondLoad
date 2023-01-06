4f31f6641719013c3fa89f64db153115e0fcdb5c
hemg2 <hemg2@naver.com>
2023-01-06 18:08:43 +0900

    PriceTextFieldCellViewModel

---
+++ SecondUpLoad/PriceTextFieldCell/PriceTextFieldCellViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  PriceTextFieldCellViewModel.swift
+//  SecondUpLoad
+//
+//  Created by 1 on 2023/01/06.
+//
+
+import RxSwift
+import RxCocoa
+import UIKit
+
+struct PriceTextFieldCellViewModel {
+    
+}
