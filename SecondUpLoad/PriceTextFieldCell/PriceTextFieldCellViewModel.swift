a4ef5e38d789b41f917784f977cc59237082592e
hemg2 <hemg2@naver.com>
2023-01-09 13:28:05 +0900

    PriceTextFieldCell UI

--- SecondUpLoad/PriceTextFieldCell/PriceTextFieldCellViewModel.swift
+++ SecondUpLoad/PriceTextFieldCell/PriceTextFieldCellViewModel.swift
@@ -10,5 +10,20 @@ import RxCocoa
 import UIKit
 
 struct PriceTextFieldCellViewModel {
+    // 모델에서 뷰로
+    let showFreeShareButton: Signal<Bool>
+    let resetPrice: Signal<Void>
     
+    
+    let priceValue = PublishRelay<String?>()
+    let freeShareButtonTapped = PublishRelay<Void>()
+    
+    init() {
+        self.showFreeShareButton = Observable.merge(priceValue.map {
+            $0 ?? "" == "0"
+        }, freeShareButtonTapped.map { _ in false }) .asSignal(onErrorJustReturn: false)
+        
+        self.resetPrice = freeShareButtonTapped
+            .asSignal(onErrorSignalWith: .empty())
+    }
 }
