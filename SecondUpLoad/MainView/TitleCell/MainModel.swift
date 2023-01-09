adc757f672653808e29cf7d28c4a254735657801
hemg2 <hemg2@naver.com>
2023-01-09 14:03:45 +0900

    MainViewModel -> MainModel ago

---
+++ SecondUpLoad/MainView/TitleCell/MainModel.swift
@@ -0,0 +1,15 @@
+//
+//  MainModel.swift
+//  SecondUpLoad
+//
+//  Created by 1 on 2023/01/09.
+//
+
+import Foundation
+
+
+struct MainModel {
+    func setAlert(errorMessage: [String]) -> Alert {
+        
+    }
+}
