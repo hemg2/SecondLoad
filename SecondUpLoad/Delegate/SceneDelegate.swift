024eb1b1e08663fc89af3feef98d38ba8627ebbd
hemg2 <hemg2@naver.com>
2023-01-09 15:18:53 +0900

    MVVM

--- SecondUpLoad/Delegate/SceneDelegate.swift
+++ SecondUpLoad/Delegate/SceneDelegate.swift
@@ -14,11 +14,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = scene as? UIWindowScene else { return }
-        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: windowScene)
+        
         let rootViewConteoller = MainViewController()
-//        rootViewConteoller.bind(rootViewModel)
+        rootViewConteoller.bind(rootViewModel)
         window?.rootViewController = UINavigationController(rootViewController: rootViewConteoller)
         window?.makeKeyAndVisible()
     }
